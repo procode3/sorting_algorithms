fc36a8aeae76107b3a740df6ea3bb6cd80f07052
procode3 <wr.t8r1@gmail.com>
2023-01-20 16:41:58 +0300

    Fixed header file

--- sort.h
+++ sort.h
@@ -40,6 +40,7 @@ void print_list(const listint_t *list);
 /* Sorting algoritms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+int find_min(int *a, int size, int index);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
